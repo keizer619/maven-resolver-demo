8006b91191c8e8841ed2c8a5d2040203ad74c2fa
Tharik Kanaka <tharik.kanaka@gmail.com>
2020-04-27 18:59:25 +0530

    Include repository authentication

--- src/main/java/resolve/MavenResolver.java
+++ src/main/java/resolve/MavenResolver.java
@@ -3,11 +3,11 @@ package resolve;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.Authentication;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactRequest;
@@ -17,6 +17,7 @@ import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.eclipse.aether.util.repository.AuthenticationBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,37 +26,57 @@ public class MavenResolver {
 
     private List<RemoteRepository> repositories;
     RepositorySystem system;
-    RepositorySystemSession session;
+    DefaultRepositorySystemSession session;
 
+    /**
+     * Resolver will be initialized to specified to location.
+     * @param targetLocation file path the target
+     */
     public MavenResolver(String targetLocation) {
-
         DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
         locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
         locator.addService(TransporterFactory.class, FileTransporterFactory.class);
         locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
-
-        locator.setErrorHandler(new DefaultServiceLocator.ErrorHandler() {
-            @Override
-            public void serviceCreationFailed(Class<?> type, Class<?> impl, Throwable exception) {
-                exception.printStackTrace();
-            }
-        });
-
         system = locator.getService(RepositorySystem.class);
-        DefaultRepositorySystemSession defaultSession = MavenRepositorySystemUtils.newSession();
+        session = MavenRepositorySystemUtils.newSession();
         LocalRepository localRepo = new LocalRepository(targetLocation);
-        defaultSession.setLocalRepositoryManager(system.newLocalRepositoryManager(defaultSession, localRepo));
-        session = defaultSession;
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
 
         repositories = new ArrayList<>();
         repositories.add(new RemoteRepository.Builder(
                 "central", "default", "https://repo.maven.apache.org/maven2/").build());
     }
 
-    public void addRepository(String id, String location) {
-        repositories.add(new RemoteRepository.Builder(id, "default", location).build());
+    /**
+     * Sepcified repository will be added to remote repositories.
+     * @param id identifier of the repository
+     * @param url url of the repository
+     */
+    public void addRepository(String id, String url) {
+        repositories.add(new RemoteRepository.Builder(id, "default", url).build());
     }
 
+    /**
+     * Sepcified repository will be added to remote repositories.
+     * @param id identifier of the repository
+     * @param url url of the repository
+     */
+    public void addRepository(String id, String url, String username, String password) {
+        Authentication authentication =
+                new AuthenticationBuilder()
+                        .addUsername(username)
+                        .addPassword(password)
+                        .build();
+        repositories.add(new RemoteRepository.Builder(id, "default", url)
+                .setAuthentication(authentication).build());
+    }
+
+    /**
+     * Resolves provided dependency into resolver location.
+     * @param groupId group ID of the dependency
+     * @param artifactId artifact ID of the dependency
+     * @param version version of the dependency
+     */
     public void resolve(String groupId, String artifactId, String version) {
         Artifact artifact = new DefaultArtifact(groupId + ":" + artifactId + ":" + version);
         ArtifactRequest artifactRequest = new ArtifactRequest();
--- src/main/java/resolve/Run.java
+++ src/main/java/resolve/Run.java
@@ -5,10 +5,16 @@ public class Run {
         MavenResolver resolver = new MavenResolver("target/local-repo");
 
         //Pass local .m2 directory as a repository
-        resolver.addRepository("local", "file:~/.m2/repository/");
+        resolver.addRepository("local", "file:" + System.getProperty("user.home") + "/.m2/repository");
 
         //Pass custom maven repository
         resolver.addRepository("wso2-releases", "http://maven.wso2.org/nexus/content/repositories/releases/");
+
+
+        String username = System.getenv("GIT_USER");
+        String password = System.getenv("GIT_PAT");
+        resolver.addRepository("ballerina-github",
+                "https://maven.pkg.github.com/ballerina-platform/ballerina-update-tool", username, password);
 
         //This is available in maven central which is the default repository
         resolver.resolve("org.apache.spark", "spark-core_2.12", "2.4.5");
@@ -18,5 +24,8 @@ public class Run {
 
         //This is available in local m2 repository
         resolver.resolve("org.tharik", "my-sample-prog", "0.9-SNAPSHOT");
+
+        //This is available in private repostory
+        resolver.resolve("org.ballerinalang", "ballerina-command-distribution", "0.8.5");
     }
 }
